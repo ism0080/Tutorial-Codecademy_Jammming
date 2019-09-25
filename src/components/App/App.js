bf4f080c204407e1289bd7e2400c58720d0f2507
Isaac Mackle <isaac.mackle@gmail.com>
2019-09-25 17:33:49 +1000

    Remove song from playlist - Works in code but not visually

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -42,18 +42,25 @@ class App extends React.Component {
     };
 
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
     let tracks = this.state.playlistTracks;
     if (tracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
-    } else {
+    } 
       tracks.push(track);
       this.setState = ({
         playlistTracks: tracks
       })
-    }
+
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState = ({playlistTracks: tracks});
   }
 
   render() {
@@ -63,8 +70,11 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <SearchResults searchResults={this.state.searchResults} 
+                            onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} 
+                      playlistTracks={this.state.playlistTracks} 
+                      onRemove={this.removeTrack}/>
           </div>
         </div>
       </div>

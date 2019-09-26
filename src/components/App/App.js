75331ae06e389f954a5cdb3bc0b6dee83875fa66
Isaac Mackle <isaac.mackle@gmail.com>
2019-09-26 12:29:14 +1000

    removed unused function call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -58,7 +58,6 @@ class App extends React.Component {
     Spotify.search(searchTerm).then(searchResults => {
       this.setState({ searchResults: searchResults });
     });
-    this.noResult();
   }
 
   render() {

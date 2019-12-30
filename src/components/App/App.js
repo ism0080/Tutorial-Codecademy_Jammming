c5acbe109283301f582be7da236564761a0606aa
Isaac Mackle <isaac.mackle@gmail.com>
2019-12-30 18:45:25 +1000

    simple way of seeing users current playlists

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
+import { UserPlayList } from '../Playlist/UserPlayList'
 import Spotify from '../../util/Spotify';
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([])
   const [playlistName, setPlaylistName] = useState('(New Playlist)')
   const [playlistTracks, setPlaylistTracks] = useState([])
+  const [userPlaylists, setUserPlaylists] = useState([])
 
   const addTrack = (track) => {
     let tracks = [...playlistTracks];
@@ -32,7 +34,7 @@ const App = () => {
   const savePlaylist = () => {
     const tracks = [...playlistTracks]
     const trackUris = tracks.map(track => track.uri);
-    if(!trackUris.length) {
+    if (!trackUris.length) {
       return window.alert('No Songs in Playlist')
     }
     Spotify.savePlaylist(playlistName, trackUris).then(() => {
@@ -47,23 +49,31 @@ const App = () => {
     });
   }
 
-    return (
-      <div>
-        <h1>&#9835; DJ <span className="highlight">Mackdaddy Playlist</span> Creator &#9835;</h1>
-        <div className="App">
-          <SearchBar onSearch={search} />
-          <div className="App-playlist">
-            <SearchResults searchResults={searchResults}
-              onAdd={addTrack} />
-            <Playlist playlistName={playlistName}
-              playlistTracks={playlistTracks}
-              onRemove={removeTrack}
-              onNameChange={updatePlaylistName}
-              onSave={savePlaylist} />
-          </div>
+  useEffect(() => {
+    Spotify.getUserPlaylists().then(playlists => {
+      setUserPlaylists(playlists)
+    })
+  })
+
+  return (
+    <div>
+      <h1>&#9835; DJ <span className="highlight">Mackdaddy Playlist</span> Creator &#9835;</h1>
+      <div className="App">
+        <SearchBar onSearch={search} />
+        <div className="App-playlist">
+          <SearchResults searchResults={searchResults}
+            onAdd={addTrack} />
+          <Playlist playlistName={playlistName}
+            userPlaylists={userPlaylists}
+            playlistTracks={playlistTracks}
+            onRemove={removeTrack}
+            onNameChange={updatePlaylistName}
+            onSave={savePlaylist} />
+          <UserPlayList playlists={userPlaylists} />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
-  export default App;
+export default App;
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -80,7 +80,39 @@ const Spotify = {
                             });
                     });
             });
-    }
+    },
+
+    getUserPlaylists() {
+
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}` };
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me',
+            {
+                headers: headers
+            }).then(response => response.json()
+            ).then(jsonResponse => {
+                userId = jsonResponse.id;
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
+                    {
+                        headers: headers,
+                        method: 'GET',
+                    }).then(response => {
+                        return response.json();
+                    }).then(jsonResponse => {
+                        if (!jsonResponse.items) {
+                            return [];
+                        }
+                        return jsonResponse.items.map((item) => ({
+                            id: item.id,
+                            name: item.name,
+                            tracks: item.tracks.total,
+                            image: item.images[0].url,
+                        }));
+                    })
+            })
+    },
 };
 
 export default Spotify;
